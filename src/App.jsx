d766ca2f001e94d79eb1a8babca7d1f2d52f66f9
Pablisson <pablissonaraujo@gmail.com>
2022-07-16 12:26:51 -0300

    Criando o component Post.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-function App() {
+import { Post } from './Post'
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Post 
+        author="Páblisson Araújo" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, saepe sint! Est fuga labore, architecto perferendis dicta adipisci expedita nobis aliquid enim exercitationem, quas, inventore molestiae vel quia reprehenderit possimus" 
+      />
+      <Post
+        author="Páblisson Araújo" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, saepe sint! Est fuga labore, architecto perferendis dicta adipisci expedita nobis aliquid enim exercitationem, quas, inventore molestiae vel quia reprehenderit possimus" 
+      />
+    </>
   )
 }
 
-export default App
---
+++ src/Post.jsx
@@ -0,0 +1,11 @@
+
+export function Post(props){
+//  console.log(props);
+return (
+    <>
+      <stron>{ props.author }</stron>
+      <p>{props.content}</p>
+    
+    </>
+  )     
+}
