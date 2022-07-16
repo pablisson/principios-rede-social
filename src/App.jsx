dd1f6cb47f597ec296d6a800be96ac8d77cc7b69
Pablisson <pablissonaraujo@gmail.com>
2022-07-16 14:15:25 -0300

    Criando o header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { Post } from './Post'
+import { Header } from './components/Header';
+import { Post } from './Post';
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post 
         author="Páblisson Araújo" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, saepe sint! Est fuga labore, architecto perferendis dicta adipisci expedita nobis aliquid enim exercitationem, quas, inventore molestiae vel quia reprehenderit possimus" 
