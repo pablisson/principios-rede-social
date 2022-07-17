09550bfde9fa69c9eb3000cb56709cd3c14a84c8
Pablisson <pablissonaraujo@gmail.com>
2022-07-17 14:59:38 -0300

    Adicionando Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,8 +11,8 @@ export function Post(){
             <span> Scrum Master</span>
           </div>
         </div>
-        
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30"></time>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
       </header>
 
       <div className={ styles.content }>
@@ -24,6 +24,16 @@ export function Post(){
           <a href="#">#nlw</a> {' '}
           <a href="#">#rocketseat</a></p>{' '}
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>      
     </article>  
   );
 }
