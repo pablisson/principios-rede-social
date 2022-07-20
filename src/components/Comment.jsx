20e30f086fc35bc5d61a929239154b65feee861e
Pablisson <pablissonaraujo@gmail.com>
2022-07-20 06:17:12 -0300

    Finalização de toda parte visual

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment(){
   return(
     <div className={styles.Comment}>
-        <img src="https://github.com/pablisson.png" />
+        <Avatar hasBorder={false} src="https://github.com/pablisson.png" />
         <div className={styles.CommentBox}>
           <div className={styles.CommentContent}>
             <header>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css'
 export function Sidebar() {
   return(
@@ -9,7 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399843-5870eea9b74e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       
         <div className={ styles.profile }>
-          <img className={ styles.avatar } src='https://github.com/pablisson.png' alt='perfil'/>
+          <Avatar src="https://github.com/pablisson.png" />
 
           <strong>Páblisson Araújo</strong>
           <span>Scrum Master</span>
